88c339fe9a3ee5e96a7810b65f263a5600e93c78
dilip23456789012-hub <dilip23456789012@gmail.com>
2026-09-24 22:50:50 +0530

    add file

---
+++ numpy/arrayfunction.py
@@ -0,0 +1,18 @@
+import numpy as np
+arr=np.arange(6)
+print(arr)
+# change the shape of the array
+arr=arr.reshape(2,3)
+print(arr)
+#for practice
+arr2=np.arange(18)
+arr2=arr2.reshape(3,6)
+print(arr2)
+# create a array
+arr3=np.zeros(5)
+print(arr3)
+arr4=np.ones(5)
+print(arr4)
+arr5=np.linspace(1,10,4)
+print(arr5)
+
